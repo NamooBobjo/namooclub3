05efc486e67fd9e2b1bfe09627466d193546fd3d
seongyong <seongyong@naver.com>
2014-04-08 10:38:04 +0900

    dd

--- namoo-club3-service/src/main/java/com/namoo/club/dao/jdbc/SocialPersonDaojdbc.java
+++ namoo-club3-service/src/main/java/com/namoo/club/dao/jdbc/SocialPersonDaojdbc.java
@@ -1,5 +1,10 @@
 package com.namoo.club.dao.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.namoo.club.dao.SocialPersonDao;
@@ -8,8 +13,20 @@ import com.namoo.club.domain.entity.SocialPerson;
 public class SocialPersonDaojdbc implements SocialPersonDao {
 
 	@Override
+	//전체 목록 조회
 	public List<SocialPerson> readAllPerson() {
-		// TODO Auto-generated method stub
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet resultSet = null;
+		List<SocialPerson> persons = new ArrayList<SocialPerson>();
+		
+		try {
+			conn = DbConnection.getConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 
@@ -37,4 +54,23 @@ public class SocialPersonDaojdbc implements SocialPersonDao {
 
 	}
 
+	
+	//소멸 메소드
+	private void quiet(ResultSet resultSet, PreparedStatement pstmt, Connection conn) {
+		if(resultSet!=null){
+			try {resultSet.close();} catch (SQLException e) {e.printStackTrace();}
+		}
+		quiet(pstmt,conn);
+	}
+	
+	private void quiet(PreparedStatement pstmt, Connection conn) {
+	
+		if(pstmt !=null){	
+			try {pstmt.close();} catch (SQLException e) {e.printStackTrace();}
+		}
+		
+		if(conn!=null){
+			try {conn.close();} catch (SQLException e) {e.printStackTrace();}
+		}	
+	}
 }
