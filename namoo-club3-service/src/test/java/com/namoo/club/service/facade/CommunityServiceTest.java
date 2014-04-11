f064bc1e5a489382e5d95fb8964bacafc1d2a20c
j335632 <j335632@hotmail.com>
2014-04-11 15:42:24 +0900

    aaa

---
+++ namoo-club3-service/src/test/java/com/namoo/club/service/facade/CommunityServiceTest.java
@@ -0,0 +1,89 @@
+package com.namoo.club.service.facade;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CommunityServiceTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testRegistCommunityStringStringStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRegistCommunityStringStringStringListOfCategory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindCommunity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testJoinAsMemberStringStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testJoinAsMemberStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindAllCommunities() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindCommunityMember() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindAllCommunityMember() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCountMembers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveCommunity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindJoinedCommunities() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindAllUnjoinedCommunities() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindManagedCommnities() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWithdrawalCommunity() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ namoo-club3-service/src/test/java/com/namoo/club/service/facade/TownerServiceTest.java
@@ -0,0 +1,47 @@
+package com.namoo.club.service.facade;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.namoo.club.shared.DbCommonTest;
+
+public class TownerServiceTest extends DbCommonTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testLoginAsTowner() {
+		//
+		
+	}
+
+	@Test
+	public void testRegistTowner() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveTowner() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindTowner() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindAllTowner() {
+		fail("Not yet implemented");
+	}
+
+}
